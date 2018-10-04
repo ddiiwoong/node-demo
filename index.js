e9f03f82272f0b6a5f6c9713b334c65dc9965c93
Jinwoong Kim <rokakjw@gmail.com>
2018-10-04 10:40:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+//app.get('/', function (req, res) {
   //res.send('Hello World v1.5.0!');
-  res.set(‘Content-Type’, ‘text/html’);
-  res.send(new Buffer(‘<h2>Test String</h2>’));
+  //res.set(‘Content-Type’, ‘text/html’);
+  //res.send(new Buffer(‘<h2>Test String</h2>’));
+//});
+
+app.get("/", function(req, res, next) {
+  var msg = 'Hello World v1.5.2';
+  var target = 'blue'
+  res.send('<body style="background:' + target + '; color: white; font-size:100px;">' + msg + '</body>');
 });
 
 var server = app.listen(3000, function () {
